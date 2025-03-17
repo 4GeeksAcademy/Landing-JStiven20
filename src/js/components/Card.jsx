97008970dde5169732044cca5e7ebdd969eab048
JStiven20 <stivenf.gil@gmail.com>
2025-03-17 18:09:29 +0000

    Landing-JStiven20

---
+++ src/js/components/Card.jsx
@@ -0,0 +1,53 @@
+const Card = () => {
+  return (
+
+
+    <div style={{ display: "flex", justifyContent: "center", gap: "20px", marginTop: "20px" }}>
+
+      <div className="card" style={{ width: "18rem" }}>
+        <img src="https://img.freepik.com/foto-gratis/ensalada-papaya-servida-fideos-arroz-ensalada-verduras-decorada-ingredientes-comida-tailandesa_1150-26509.jpg?t=st=1742234850~exp=1742238450~hmac=fe890ad1f8bc3a69b76d5509afc9dbcb2ce69deb730896119006b97c78b86372&w=1380" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+
+      <div className="card" style={{ width: "18rem" }}>
+        <img src="https://img.freepik.com/foto-gratis/ensalada-papaya-servida-fideos-arroz-ensalada-verduras-decorada-ingredientes-comida-tailandesa_1150-26509.jpg?t=st=1742234850~exp=1742238450~hmac=fe890ad1f8bc3a69b76d5509afc9dbcb2ce69deb730896119006b97c78b86372&w=1380" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+
+      <div className="card" style={{ width: "18rem" }}>
+        <img src="https://img.freepik.com/foto-gratis/ensalada-papaya-servida-fideos-arroz-ensalada-verduras-decorada-ingredientes-comida-tailandesa_1150-26509.jpg?t=st=1742234850~exp=1742238450~hmac=fe890ad1f8bc3a69b76d5509afc9dbcb2ce69deb730896119006b97c78b86372&w=1380" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+
+      <div className="card" style={{ width: "18rem" }}>
+        <img src="https://img.freepik.com/foto-gratis/ensalada-papaya-servida-fideos-arroz-ensalada-verduras-decorada-ingredientes-comida-tailandesa_1150-26509.jpg?t=st=1742234850~exp=1742238450~hmac=fe890ad1f8bc3a69b76d5509afc9dbcb2ce69deb730896119006b97c78b86372&w=1380" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Card;
